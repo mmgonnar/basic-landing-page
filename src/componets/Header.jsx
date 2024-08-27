c4fe7251d1d25f48e00d43427d262f8897554b33
mmgonnar <gn.mariela@gmail.com>
2024-08-27 09:58:58 -0600

    Fixing Header css

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -3,13 +3,20 @@ import React from "react";
 const Header = () => {
   return (
     <section className="header">
-      <img src="https://picsum.photos/150/50" alt="" className="header__logo" />
-      <div className="header__container">
-        <ul>
-          <li>Link 1</li>
-          <li>Link 1</li>
-          <li>Link 1</li>
-        </ul>
+      <div className="header__content">
+        {/* <img
+          src="https://picsum.photos/150/50"
+          alt=""
+          className="header__logo"
+        /> */}
+        <h1 className="header__logo">Header Logo</h1>
+        <div className="header__container">
+          <ul>
+            <li>Link 1</li>
+            <li>Link 1</li>
+            <li>Link 1</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
